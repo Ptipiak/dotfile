e699e21eedac248f8748a73596cd34d8e513a39c
Ptipiak <Ptipiak.off@gmail.com>
2024-02-18 10:56:04 +0800

    Adding a plugin to better use diff split.

---
+++ nvim/lua/plugins/diff-view.lua
@@ -0,0 +1,9 @@
+----------------------------------------------------------------------------
+-- Diff view
+-- convenient way to quickly bring up all modified files in a diffsplit.
+----------------------------------------------------------------------------
+return {
+	enabled = true,
+	version = false,
+	"sindrets/diffview.nvim"
+}
