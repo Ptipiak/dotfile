df55f84c16d196fd351d1f10cb34ea4eb5d0d8d0
Ptipiak <Ptipiak.off@gmail.com>
2024-02-28 17:14:52 +0800

    Adding a plugin to have better indentaiton and folding behaviours

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,34 @@
+----------------------------------------------------------------------------
+-- indent-blankline better folding and indentation
+----------------------------------------------------------------------------
+---@type LazyPluginSpec
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = "VeryLazy",
+  opts = {
+    indent = {
+      char = "▏", -- Thiner, not suitable when enable scope
+      tab_char = "▏",
+    },
+    scope = {
+      -- Rely on treesitter, bad performance
+      enabled = false,
+      -- highlight = highlight,
+    },
+  },
+  config = function(_, opts)
+    local ibl = require("ibl")
+    local hooks = require("ibl.hooks")
+
+    ibl.setup(opts)
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+    -- Hide first level indent, using `foldsep` to show it
+    hooks.register(hooks.type.VIRTUAL_TEXT, function(_, _, _, virt_text)
+      if virt_text[1] and virt_text[1][1] == opts.indent.char then
+        virt_text[1] = { " ", { "@ibl.whitespace.char.1" } }
+      end
+      return virt_text
+    end)
+  end,
+}
