d6b049c1ddbfe0af372b5192cfe1b79f763523b3
Ptipiak <Ptipiak.off@gmail.com>
2024-11-09 20:43:38 +0800

    Adding `zoxyde` to the tool set

--- profile/fish/config.fish
+++ profile/fish/config.fish
@@ -1,5 +1,12 @@
-if status is-interactive
+function third_party
   pyenv init - | source
-  alias l "ls -al"
-  nvm use
+  zoxide init fish | source
+  fnm env --use-on-cd | source
+end
+
+if status is-interactive
+  # set -U $fish_greeting
+  third_party
+  alias l "ls -al -h"
+  source ~/.config/fish/functions/greeting.fish
 end
