16a306ce6a592048f3309687d7c41280d5521b05
Ptipiak <Ptipiak.off@gmail.com>
2024-11-09 20:38:34 +0800

    Fixing issue with `Coq` and terminal autocmds.
    The parameter `resolve_timeout = 0.40` is needed as some LSP servers are
    particulary slow to answer and `Coq` wasn't displaying the documentation
    linked with the completion list.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,17 +4,29 @@
 
 -- Make the terminal emulator look better
 terminal_group = vim.api.nvim_create_augroup("terminal", {})
-vim.api.nvim_create_autocmd("TermOpen", {
+vim.api.nvim_create_autocmd("TermEnter", {
   desc = "Make terminal emulator look like a terminal",
   group = terminal_group,
   callback = function()
     local lines = vim.api.nvim_buf_get_lines(0, -2, -1, true)
+    -- length(lines)
     vim.api.nvim_win_set_cursor(0, { 4, 1 })
     vim.wo.number = false
     vim.wo.relativenumber = false
     vim.wo.scrolloff=0
     vim.wo.signcolumn = "auto" -- left column sign and size
     vim.wo.wrap = true
+  end
+});
+vim.api.nvim_create_autocmd("TermLeave", {
+  desc = "Make terminal emulator look like a terminal",
+  group = terminal_group,
+  callback = function()
+    vim.wo.number = true
+    vim.wo.relativenumber = true
+    vim.wo.scrolloff=4
+    vim.wo.signcolumn = "auto:1-4" -- left column sign and size
+    vim.wo.wrap = false
   end
 });
 
--- nvim/lua/plugins/completion/coq.lua
+++ nvim/lua/plugins/completion/coq.lua
@@ -1,23 +1,33 @@
+----------------------------------------------------------------------------
+-- Coq
+----------------------------------------------------------------------------
 return {
   enabled = true,
-  lazy = false,
-  priority = 20,
   'ms-jpq/coq_nvim',
+  event = {"VeryLazy"},
   branch = 'coq',
   name = 'coq',
   opts =  {
     display = {
+      preview = {
+        resolve_timeout = 0.40,
+        positions = { east= 1, north= 2, south= 3, west= 4 },
+        border = "double"
+      },
       pum = {
-        fast_close = false
-      }
+        fast_close = false,
+      },
+      icons = {
+        mode = "short",
+      },
     },
-    args = {
-      "--shut-up"
-    }
+   -- keymap = {
+   --   eval_snips = '<leader>j'
+   -- },
   },
   config = function(_, opts)
     local coq = require('coq')
-    coq.Now(unpack(opts.args))
-    vim.g.coq_settings = unpack(opts.display)
+    coq.Now("--shut-up")
+    vim.g.coq_settings = opts
   end
 }
